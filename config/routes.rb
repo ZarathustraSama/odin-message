27c5b72e0264d427d22b3aaff6d2955d2f0a6de8
ZarathustraSama <mr.gabriel@live.it>
2025-06-04 15:03:33 +0200

    Added homepage controller with routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'homepage#index'
   devise_for :users, controllers: { sessions: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
