9cfa46c902ed94aa04bb80d00aef22017e805bd5
ZarathustraSama <mr.gabriel@live.it>
2025-06-13 17:00:18 +0200

    Added validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ApplicationRecord
+  validates :sender, :class_name => 'User', presence: true
+  validates :recipient, :class_name => 'User', presence: true
+  validates :content, presence: true
+
   belongs_to :sender, :class_name => 'User'
   belongs_to :recipient, :class_name => 'User'
 end
