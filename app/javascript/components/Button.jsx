9e91ba07f446e418ebaa2936247a91f404a37bc0
ZarathustraSama <mr.gabriel@live.it>
2025-06-19 16:22:25 +0200

    Reformatted component

--- app/javascript/components/Button.jsx
+++ app/javascript/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Button({ label }) {
+function Button({ value, onClick }) {
   return (
     <>
-      <button type="button">{label}</button>
+      <button type="button" value={value} onClick={onClick} />
     </>
   )
 }
