c638740980f1cb61d25cff5b68cf9abc1743ff02
ZarathustraSama <mr.gabriel@live.it>
2025-06-19 16:38:39 +0200

    Added methods for retrieving other/personal user data

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    users = User.where.not(id: current_user.id).order(username: :asc).
+    render json: users
+  end
+
+  def show
+    render json: current_user
+  end
+end
