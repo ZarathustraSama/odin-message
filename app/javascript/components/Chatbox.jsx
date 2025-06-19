1189661ebca1d2dca15dd6cb7b88c59079459ee4
ZarathustraSama <mr.gabriel@live.it>
2025-06-19 16:41:42 +0200

    Changed imports, added useEffect for fetching all data, simplified rendered component

--- app/javascript/components/Chatbox.jsx
+++ app/javascript/components/Chatbox.jsx
@@ -2,15 +2,60 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 import MessageBox from "./MessageBox";
-import InputBox from "./InputBox";
+import Input from "./Input";
 import Button from "./Button";
+import Select from "./Select";
 
 function Chatbox() {
-  const userLabel = "User";
-  const messageLabel = "Message";
-  const buttonLabel = "Send";
-  const [user, setUser] = useState("");
+  const buttonValue = "Send";
+  const lastSelectedUser = "";
+
+  const [messages, setMessages] = useState([]);
+  const [currentUserId, setCurrentUserId] = useState(null);
+  const [users, setUsers] = useState([]);
   const [message, setMessage] = useState("");
+
+  // First we fetch the current user id
+  useEffect(() => {
+    const url = "/api/v1/users/show";
+    fetch(url)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error("Id response was not ok.")
+    })
+    .then((res) => setCurrentUserId(res.id))
+    .catch((error) => console.log(error))
+  }, [])
+
+  // Then we fetch the list of users except the current one
+  useEffect(() => {
+    const url = "/api/v1/users/index";
+    fetch(url)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error("Users response was not ok.");
+    })
+    .then((res) => setUsers(res))
+    .catch((error) => console.log(error))
+  }, [])
+
+  // Lastly we fetch the messages sent up until now between them
+  useEffect(() => {
+    const url = "/api/v1/messages/index";
+    fetch(url)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error("Messages response was not ok.");
+    })
+    .then((res) => setMessages(res))
+    .catch((error) => console.log(error))
+  }, [])
 
   function handleClick(e) {
     e.preventDefault();
@@ -18,11 +63,13 @@ function Chatbox() {
 
   return (
     <>
-      <main>
-        <MessageBox />
-        <InputBox label={userLabel} onChange={setUser} />
-        <InputBox label={messageLabel} onChange={setMessage}/>
-        <Button label={buttonLabel} onClick={handleClick} />
+      <main className="chatbox">
+        <MessageBox messages={messages} />
+        <form action="">
+          <Select value={lastSelectedUser} options={users} setOption={setUsers} />
+          <Input value={message} setValue={setMessage} />
+          <Button value={buttonValue} onClick={handleClick} />
+        </form>
       </main>
     </>
   )
