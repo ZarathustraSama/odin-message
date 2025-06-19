eebaf24b1be62eeb3c4e54d18c0a709fa52877b2
ZarathustraSama <mr.gabriel@live.it>
2025-06-19 16:37:55 +0200

    Changed criteria for which messages to get, added method for creating messages, added auth check

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,22 @@
 class Api::V1::MessagesController < ApplicationController
+  before_action :authenticate_user!
   def index
+    messages = Message.sender_id(current_user.id).order(created_at:)
+    render json: messages
   end
 
   def create
+    message = Message.create!(message_params)
+    if message
+      render json: message
+    else
+      render json: message.errors
+    end
+  end
+
+  private
+
+  def message_params
+    params.permit(:sender_id, :receiver_id, :content)
   end
 end
