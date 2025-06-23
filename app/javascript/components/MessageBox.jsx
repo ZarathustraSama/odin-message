a4568713e7d73cd77180e57add927f387744cb6c
ZarathustraSama <mr.gabriel@live.it>
2025-06-23 11:02:00 +0200

    Fixed typo

--- app/javascript/components/MessageBox.jsx
+++ app/javascript/components/MessageBox.jsx
@@ -4,7 +4,7 @@ import Message from "./Message";
 
 function MessageBox ({ messages }) {
   if (messages.length > 0) {
-    return (messages.map(m => <Message key={m.id} message={m} />))
+    return (messages.map(m => <Message key={m.id} message={m.content} />))
   }
   else {
     return <span>You have yet to exchange words with this user!</span>
