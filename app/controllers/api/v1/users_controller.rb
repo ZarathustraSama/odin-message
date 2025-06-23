1f800603892ac01d80bb312ab36168891c2d8b2e
ZarathustraSama <mr.gabriel@live.it>
2025-06-23 10:26:24 +0200

    Fixed typo

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   before_action :authenticate_user!
   def index
-    users = User.where.not(id: current_user.id).order(username: :asc).
+    users = User.where.not(id: current_user.id).order(username: :asc)
     render json: users
   end
 
