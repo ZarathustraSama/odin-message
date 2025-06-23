a01ac4d99ab067093c64189b6e60cb809d50489e
ZarathustraSama <mr.gabriel@live.it>
2025-06-23 10:25:29 +0200

    Added value prop

--- app/javascript/components/Button.jsx
+++ app/javascript/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button({ value, onClick }) {
   return (
     <>
-      <button type="button" value={value} onClick={onClick} />
+      <button type="button" value={value} onClick={onClick}>{value}</button>
     </>
   )
 }
