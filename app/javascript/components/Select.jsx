3ab4a2759108f1b0ef99d60de8fa02d19080144a
ZarathustraSama <mr.gabriel@live.it>
2025-06-23 11:03:07 +0200

    Changed prop name to better reflect purpouse

--- app/javascript/components/Select.jsx
+++ app/javascript/components/Select.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function Select({ value, options, setOption }) {
+function Select({ value, options, setUserId }) {
   return (
-    <select value={value} onChange={e => setOption(e.currentTarget.value)}>
+    <select value={value} onChange={e => setUserId(e.currentTarget.value)}>
       {options.map(o => <option key={o.id} value={o.id}>{o.username}</option>)}
     </select>
   )
