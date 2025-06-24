0ccc7e4a67e6f91842f2af7dad300c396b8535de
ZarathustraSama <mr.gabriel@live.it>
2025-06-24 16:29:23 +0200

    Fixed handling of selecting another user

--- app/javascript/components/Select.jsx
+++ app/javascript/components/Select.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function Select({ value, options, setUserId }) {
+function Select({ options, setUserId }) {
   return (
-    <select id="selectUser" name="selectUser" value={value} onChange={e => setUserId(e.currentTarget.value)}>
+    <select id="selectUser" name="selectUser" onChange={e => setUserId(e.currentTarget.value)}>
       {options.map(o => <option key={o.id} value={o.id}>{o.username}</option>)}
     </select>
   )
