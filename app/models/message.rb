55aa75bd7fecba237ac57f3f277645834a84ca5a
ZarathustraSama <mr.gabriel@live.it>
2025-06-24 15:15:15 +0200

    Removed useless validators

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,4 @@
 class Message < ApplicationRecord
-  validates :sender, :class_name => 'User', presence: true
-  validates :recipient, :class_name => 'User', presence: true
   validates :content, presence: true
 
   belongs_to :sender, :class_name => 'User'
