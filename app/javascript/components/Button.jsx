8b2f704c988e292b5be8979f7befc5a20d1a5c60
ZarathustraSama <mr.gabriel@live.it>
2025-06-24 14:59:53 +0200

    Fixed typo

--- app/javascript/components/Button.jsx
+++ app/javascript/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button({ value, onClick }) {
   return (
     <>
-      <button type="button" value={value} onClick={onClick}>{value}</button>
+      <button type="button" value={value} onClick={(e) => {onClick(e)}}>{value}</button>
     </>
   )
 }
