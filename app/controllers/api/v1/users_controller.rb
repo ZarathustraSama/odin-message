28f5621a20e6e8093498f0a5c13331ac0d4f5623
ZarathustraSama <mr.gabriel@live.it>
2025-06-25 11:03:09 +0200

    Removed unused action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,8 +4,4 @@ class Api::V1::UsersController < ApplicationController
     users = User.where.not(id: current_user.id).order(username: :asc)
     render json: users
   end
-
-  def show
-    render json: current_user
-  end
 end
