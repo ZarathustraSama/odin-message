7f774ac53f48f68654d77a571848f3a1e81ab956
ZarathustraSama <mr.gabriel@live.it>
2025-06-25 16:04:34 +0200

    Added fetch for profile prop to pass to ProfileDetail component, regardless of result

--- app/javascript/components/Profile.jsx
+++ app/javascript/components/Profile.jsx
@@ -3,11 +3,31 @@ import React from "react";
 import Navbar from "./Navbar";
 import ProfileDetails from "./ProfileDetails";
 
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Profile() {
+  const params = useParams();
+  const [profile, setProfile] = useState([]);
+
+  useEffect(() => {
+    const url = `api/v1/profile/show/${params.id || ""}`;
+    fetch(url)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error("Profile response was not ok.");
+    })
+    .then((res) => setProfile(res)
+    )
+    .catch((err) => console.log(err))
+  }, [params.id]);
+
   return (
     <>
       <Navbar />
-      <ProfileDetails />
+      <ProfileDetails profile={profile}/>
     </>
   )
 }
