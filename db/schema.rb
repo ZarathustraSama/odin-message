2658337de6a7f316b5b5ec78cb8d1824a0714c9a
ZarathustraSama <mr.gabriel@live.it>
2025-06-25 10:34:37 +0200

    Added and migrated profile model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_12_141258) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_25_083030) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,16 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_12_141258) do
     t.datetime "updated_at", null: false
     t.index ["recipient_id"], name: "index_messages_on_recipient_id"
     t.index ["sender_id"], name: "index_messages_on_sender_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "nickname"
+    t.date "birthday"
+    t.text "bio"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +49,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_12_141258) do
 
   add_foreign_key "messages", "users", column: "recipient_id"
   add_foreign_key "messages", "users", column: "sender_id"
+  add_foreign_key "profiles", "users"
 end
