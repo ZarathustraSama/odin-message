6cfc5ea79c8f7d23f0656fcd7cfa43391a21ca40
ZarathustraSama <mr.gabriel@live.it>
2025-06-25 16:16:36 +0200

    Changed handling of #show based on id presence, added #create when profile is absent

--- app/controllers/api/v1/profile_controller.rb
+++ app/controllers/api/v1/profile_controller.rb
@@ -1,9 +1,30 @@
 class Api::V1::ProfileController < ApplicationController
   def show
-    #If no argument show the current user profile
-
+    if params[:id]
+      render json: @profile
+    else
+      profile = Profile.find_by(user: current_user)
+      render json: profile
+    end
   end
 
   def update
+    if params[:id] == current_user.id
+    end
+  end
+
+  def create
+    profile = Profile.create!(profile_params)
+    if profile
+      render json: profile
+    else
+      render json: profile.errors
+    end
+  end
+
+  private
+
+  def profile_params
+    params.permit(:nickname, :birthday, :bio).merge(:user => current_user)
   end
 end
