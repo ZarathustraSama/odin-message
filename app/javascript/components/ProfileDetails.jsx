12402d4ed04d5c57dec8f95f3ce29f33cfe9f09a
ZarathustraSama <mr.gabriel@live.it>
2025-06-25 16:06:10 +0200

    Added classes to html elements, returns ProfileForm if no profile was given

--- app/javascript/components/ProfileDetails.jsx
+++ app/javascript/components/ProfileDetails.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 
-function ProfileDetails() {
-  return (
-    <main>
-      <img>Profile Image</img>
-      <article></article>
-    </main>
-  )
+import ProfileForm from "./ProfileForm";
+
+function ProfileDetails({ profile }) {
+
+  if (profile) {
+    return (
+      <main className="profile">
+        <span className="nickname">{profile.nickname}</span>
+        <span className="birthday">{profile.birthday}</span>
+        <article className="bio">{profile.bio}</article>
+      </main>
+    )
+  }
+  else {
+    return (
+      <ProfileForm />
+    )
+  }
 }
 
 export default ProfileDetails;
