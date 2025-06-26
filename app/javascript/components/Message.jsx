74fbd23ed45e971e9d99e0d351c6e93912ee09b9
ZarathustraSama <mr.gabriel@live.it>
2025-06-26 11:59:19 +0200

    Added prop for adding class in order to better visualize messages sent from received ones

--- app/javascript/components/Message.jsx
+++ app/javascript/components/Message.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-function Message({ message }) {
-  return (
-    <>
-      <span className="message">{ message }</span>
-    </>
-  )
+function Message({ message, otherUserId }) {
+  if (message.sender_id == otherUserId) {
+    return (
+      <span className="message message-other">{message.content}</span>
+    )
+  }
+  else {
+    return (
+      <span className="message message-self">{message.content}</span>
+    )
+  }
 }
 
 export default Message;
--- app/javascript/components/MessageBox.jsx
+++ app/javascript/components/MessageBox.jsx
@@ -8,7 +8,7 @@ function MessageBox ({ messages, selectedUserId }) {
   let fMessages = messages.filter(m => (m.recipient_id == selectedUserId) || (m.sender_id == selectedUserId));
 
   if (fMessages.length > 0) {
-    return (fMessages.map(m => <Message key={m.id} message={m.content} />))
+    return (fMessages.map(m => <Message key={m.id} message={m} otherUserId={selectedUserId} />))
   }
   else {
     return <span>You have yet to exchange words with this user!</span>
